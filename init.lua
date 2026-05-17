e455041021b9e3c5463a698301d6182566a4aec7
Shadman Syed <shadman3d@gmail.com>
2026-05-17 00:02:16 -0400

    New simple config

--- init.lua
+++ init.lua
@@ -1,45 +1,49 @@
 -- Techno's Nvim Config
 
+-- Early UI2 enable
+require("vim._core.ui2").enable({})
+
 -- Use space as leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.opt.relativenumber = true
 vim.opt.showmode = false
+
 -- Saves undo history
 -- vim.opt.undofile = true
 
 -- Decrease mapped sequence wait time
 vim.opt.timeoutlen = 400
 
-vim.opt.inccommand = 'split'
--- vim.opt.cursorline = true
+-- vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
 
 vim.opt.scrolloff = 10
+-- This should make escape in normal mode remove search highlight
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Tab options
-vim.opt.tabstop = 4
+vim.opt.tabstop = 4 -- Default tab of 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.smartindent = true
+vim.opt.shiftwidth = 4 -- `>>` shifting shifts by 4
+vim.opt.expandtab = true -- Expand tab to spaces !!
+vim.opt.smartindent = true -- Follows indenting of file
 
 -- Terminal Stuff
-vim.opt.termguicolors = true
+vim.opt.termguicolors = true -- For 24bit color support
 
 
--- Movement options
+-- Movement options (between splits)
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>",  "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
--- Use netrw
-vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
+-- Use netrw as filebrowser (maybe switch to oil?)
+vim.keymap.set("n", "<leader>fe", "<CMD>Oil<CR>")
 
--- The tab options
+-- The tab/buffer keybinds
 vim.keymap.set("n", "<tab>",  ":bnext<CR>")
 vim.keymap.set("n", "<leader>bn",  ":bnext<CR>")
 vim.keymap.set("n", "<S-tab>",  ":bprevious<CR>")
@@ -48,10 +52,36 @@ vim.keymap.set("n", "<leader>bd",  ":bd<CR>")
 vim.keymap.set("n", "<leader>bx",  ":bd!<CR>")
 vim.keymap.set("n", "<leader>bl",  ":buffers<CR>")
 
--- Setting up lazy.nvim
-require("config.lazy")
+-- Completion menu
+-- vim.opt.completeopt = {"menu"}
+
+-- Vim Pack!!
+vim.pack.add({
+    { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
+--    "https://github.com/nvim-telescope/telescope.nvim",
+    "https://github.com/mason-org/mason.nvim",
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/stevearc/oil.nvim"
+})
+
+-- Builtins
+vim.cmd.packadd("nvim.undotree")
 
 -- Telescope stuff
-require("telescope").setup()
-vim.keymap.set("n", "<leader>bf", require("telescope.builtin").buffers)
-vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+-- require("telescope").setup()
+-- vim.keymap.set("n", "<leader>bf", require("telescope.builtin").buffers)
+-- vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+
+-- Theme setup
+require("rose-pine").setup()
+vim.cmd("colorscheme rose-pine")
+
+-- Oil for files
+require("oil").setup()
+
+-- Mason setup for LSPs
+require("mason").setup()
+
+-- Enable lsps
+-- nvim-lspconfig handles the options
+vim.lsp.enable({"pyright", "emmylua_ls", "rust-analyzer", "clangd"})
